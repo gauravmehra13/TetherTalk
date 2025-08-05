452c0534d321eb7239dbe1b830541b5b761820b6
gauravmehra13 <gauravmehra2470@gmail.com>
2025-08-05 15:01:04 +0530

    socket fix try 3

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    credentials: true,
   },
 });
 
